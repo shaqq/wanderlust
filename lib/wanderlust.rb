6312c2cbce07d6735956db6a0636e165a250e558
Shak <shakerislam@gmail.com>
2015-10-20 10:59:03 -0500

    make sure people are not running in prod, and have AR

--- lib/wanderlust.rb
+++ lib/wanderlust.rb
@@ -1,5 +1,33 @@
 require "wanderlust/version"
 
 module Wanderlust
-  # Your code goes here...
+  class << self
+    def safe_zone?
+      unless %w(test development dev local).include? ENV['RACK_ENV']
+        puts <<-EOF
+
+Careful! Since Wanderlust inserts a bunch of timezone data into
+your MySQL instance, it should only be used in your local test or
+development environments.
+
+        EOF
+        return false
+      end
+
+      unless defined?(::ActiveRecord)
+        puts <<-EOF
+
+Sorry! Wanderlust only works with ActiveRecord at the moment
+
+¯\\_(ツ)_/¯
+
+Submit a PR on Github if you'd like more robust support
+
+        EOF
+        return false
+      end
+
+      return true
+    end
+  end
 end
